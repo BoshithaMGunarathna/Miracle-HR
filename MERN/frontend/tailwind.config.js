009b9b4c07cc12fcb62e14581fc1f1309853e12f
Oshadi Irugalbandara <86184405+IBOshadi@users.noreply.github.com>
2024-10-21 13:23:52 +0530

    Add files via upload

---
+++ MERN/frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the path according to your file structure
+    './public/index.html', // Add any other paths where your templates are stored
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
