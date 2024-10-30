bf1ef1eb3237d3dd1e9e289bfc5b3c8873fd675e
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-30 13:33:13 +0530

    Update server.js

--- miracle-hr-backend/server.js
+++ miracle-hr-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 
