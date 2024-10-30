1197b1f22308ad10381027f746e1f96cf6e1ee8d
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-30 13:03:52 +0530

    Create authController.js

---
+++ miracle-hr-backend/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const user = new User({ name, email, password, role });
+    await user.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).send('Invalid credentials');
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { register, login };
