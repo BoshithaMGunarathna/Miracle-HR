28ff15a28aba60356f4c776f62834c6b5e5b0c6e
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-30 13:33:16 +0530

    Update userRoutes.js

--- miracle-hr-backend/routes/userRoutes.js
+++ miracle-hr-backend/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { getUserProfile, updateUserProfile } = require('../controllers/userController');
+const {
+    
+    getAllUsers,
+    getUserProfile,
+    updateUserProfile,
+    deleteUser,
+  } = require('../controllers/userController');
 const authenticateToken = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/profile', authenticateToken, getUserProfile);
-router.put('/profile', authenticateToken, updateUserProfile);
+router.get('/',getAllUsers);
+router.get('/:id', authenticateToken, getUserProfile);
+router.put('/:id', authenticateToken, updateUserProfile);
+router.delete('/:id', authenticateToken, deleteUser);
+
 
 module.exports = router;
