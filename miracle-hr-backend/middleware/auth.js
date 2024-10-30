5a602a06d3c9ffc802410f934d491c32f8c66f4e
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-30 13:33:19 +0530

    Update auth.js

--- miracle-hr-backend/middleware/auth.js
+++ miracle-hr-backend/middleware/auth.js
@@ -4,13 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authenticateToken = (req, res, next) => {
+  // Extract token from Authorization header
   const token = req.header('Authorization')?.split(" ")[1];
   if (!token) return res.status(401).send('Access Denied');
 
+  // Verify the token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).send('Invalid Token');
-    req.user = user;
-    next();
+    
+    req.user = user; // Attach user info to the request object
+    next(); // Call the next middleware or route handler
   });
 };
 
