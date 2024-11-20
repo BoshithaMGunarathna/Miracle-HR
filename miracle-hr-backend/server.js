59f27d5020b12fd8e1b4e3d6cdb841d49ee9b8d0
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-20 14:12:39 +0530

    Update server.js

--- miracle-hr-backend/server.js
+++ miracle-hr-backend/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const leaveRoutes = require('./routes/leaveRoutes');
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/leave', leaveRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
