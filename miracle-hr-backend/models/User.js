656345b707e0f8618ef9f13fb56844323fcd50ba
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-20 11:13:55 +0530

    Update User.js

--- miracle-hr-backend/models/User.js
+++ miracle-hr-backend/models/User.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  bio: { type: String, required: true }, 
+  phone: { type: String, required: true }, 
   role: { type: String, enum: ['employee', 'admin'], default: 'employee' },
 });
 
@@ -18,4 +20,4 @@ userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('users', userSchema);
