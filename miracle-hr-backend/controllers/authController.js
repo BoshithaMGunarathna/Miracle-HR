e7c040818956a8110ab352c2eedb73197ae13fcb
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-11-20 11:13:51 +0530

    Update authController.js

--- miracle-hr-backend/controllers/authController.js
+++ miracle-hr-backend/controllers/authController.js
@@ -6,14 +6,36 @@ dotenv.config();
 
 const register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
-    const user = new User({ name, email, password, role });
+    const { name, email, password, role, bio, phone } = req.body;
+    console.log(req.body);
+    if (!name || !email || !password || !bio || !phone) {
+      return res.status(400).send('Name, email, password, bio, and phone are required.');
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send('User with this email already exists.');
+    }
+
+    // Create a new user instance
+    const user = new User({ 
+      name, 
+      email, 
+      password, 
+      role: role || 'employee',
+      bio, 
+      phone
+     
+    });
+
+    // Save the user to the database
     await user.save();
+
     res.status(201).send('User registered successfully');
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(500).send(error.message);
   }
 };
+
 
 const login = async (req, res) => {
   try {
@@ -24,7 +46,18 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token });
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        bio: user.bio,
+        phone: user.phone
+     
+      },
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
