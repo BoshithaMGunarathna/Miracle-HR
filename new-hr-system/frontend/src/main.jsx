a943d4f4cd60728dba66d5b015b29dca768bea6e
BoshithaGunarathna <IT21319792@my.sliit.lk>
2024-12-19 11:10:18 +0530

    Update main.jsx

--- new-hr-system/frontend/src/main.jsx
+++ new-hr-system/frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { AuthProvider } from './pages/AuthContext.jsx'; // Import AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  {/* Wrap App with AuthProvider */}
+      <App />
+
   </React.StrictMode>
 );
-
-
-
-
-
-
